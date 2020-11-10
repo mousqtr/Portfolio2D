ae7af69d7a35188d4120a6fc2ac25bc6abfa09ca
mousqtr <49991720+mousqtr@users.noreply.github.com>
2020-11-10 21:53:48 +0100

    Hide and show the project divs

--- js/script1.js
+++ js/script1.js
@@ -1,20 +1,42 @@
 
 /* Make the menu out for mobile version */ 
 var m_menu = document.getElementById("menu");
-var m_up = document.getElementById("up");
-var m_down = document.getElementById("down");
+var m_up = document.getElementById("up-icon");
+var m_down = document.getElementById("down-icon");
+var m_contact = document.getElementById("contact-icon");
 var widthOutput = document.querySelector('#width');
 var mobileWidth = 1000;
+
+var div_progress = document.getElementById("progress");
+var div_games = document.getElementById("games");
+var div_image = document.getElementById("image");
+var div_transmission = document.getElementById("transmission");
+var div_electronic = document.getElementById("electronic");
+var div_internship = document.getElementById("internship");
+var div_website = document.getElementById("website");
+var div_others = document.getElementById("others");
+var projects = [div_progress, div_games, div_image, div_transmission, div_electronic, div_internship, div_website, div_others];
+
+
+function open_project(num_div){
+  if (projects[num_div].style.display == "none"){
+    projects[num_div].style.display = "block";
+  } else {
+    projects[num_div].style.display = "none";
+  }
+}
 
 function myFunction() {
   if (m_menu.style.display === "block") {
     m_menu.style.display = "none";
     m_up.style.display = "none";
     m_down.style.display = "block";
+    m_contact.style.display = "block";
   } else {
     m_menu.style.display = "block";
     m_up.style.display = "block";
     m_down.style.display = "none";
+    m_contact.style.display = "none";
   }
 }
 
@@ -31,25 +53,3 @@ function resize() {
 window.onresize = resize;
 
 
-/* Central button control */
-var centralButton = document.getElementById("button-central");
-function myButtonFunction() {
-    centralButton.style.backgroundColor  = "#FF0000";
-}
-
-/* Devis button */
-var devisButton = document.getElementById("devis-button");
-function myDevisButtonFunction() {
-    devisButton.style.backgroundColor  = "#FF0000";
-}
-
-
-
-
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
